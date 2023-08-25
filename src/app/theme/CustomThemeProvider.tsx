061a0de2087c613c3ec7e211aa94e8ddbe44d493
joeban <joeanddaneil5@gmail.com>
2023-08-25 18:09:35 +0800

    selectTheme to change color

--- src/app/theme/CustomThemeProvider.tsx
+++ src/app/theme/CustomThemeProvider.tsx
@@ -2,9 +2,13 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import { themeConfig } from "./theme";
+import { useAppSelector } from "../hook/hooks";
 
 const CustomThemeProvider = ({ children }: { children: React.ReactNode }) => {
-  return <ThemeProvider theme={themeConfig("")}>{children}</ThemeProvider>;
+  const { selectTheme } = useAppSelector((state) => state.persistStores);
+  return (
+    <ThemeProvider theme={themeConfig(selectTheme)}>{children}</ThemeProvider>
+  );
 };
 
 export default CustomThemeProvider;
