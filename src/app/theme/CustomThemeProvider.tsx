4f1f4182a139152d86ae5a3fcf9febc70ce2abef
joeban <joeanddaneil5@gmail.com>
2023-08-28 18:29:10 +0800

    - add default theme

--- src/app/theme/CustomThemeProvider.tsx
+++ src/app/theme/CustomThemeProvider.tsx
@@ -4,7 +4,11 @@ import React from "react";
 import { ThemeProvider } from "next-themes";
 
 const CustomThemeProvider = ({ children }: { children: React.ReactNode }) => {
-  return <ThemeProvider enableSystem={false}>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider enableSystem={false} defaultTheme="dark">
+      {children}
+    </ThemeProvider>
+  );
 };
 
 export default CustomThemeProvider;
